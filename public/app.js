3748bed958cb4c282ad6f887614d4fc0c00ebc0d
Messer <mikle0497@gmail.com>
2018-01-19 12:00:12 +0200

    update

--- public/app.js
+++ public/app.js
@@ -2,12 +2,10 @@ new Vue({
     el: '#app',
 
     data: {
-        ws: null, // Our websocket
-      //  newMsg: '', // Holds new messages to be sent to the server
-       // chatContent: '', // A running list of chat messages displayed on the screen
-        amount: null, // Email address used for grabbing an avatar
-        adress: null, // Our username
-       // joined: false // True if email and username have been filled in
+        ws: null, 
+        amount: null, 
+        adress: null,
+       
     },
 
     created: function() {
--- src/main.go
+++ src/main.go
@@ -6,35 +6,31 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-//var clients = make(map[*websocket.Conn]bool) // connected clients
-var broadcast = make(chan Message)           // broadcast channel
 
-// Configure the upgrader
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-// Define our message object
+
 type Message struct {
 	Amount    string `json:"amount"`
 	Adress string `json:"adress"`
-	//Message  string `json:"message"`
 }
 
 func main() {
-	// Create a simple file server
+	
 	fs := http.FileServer(http.Dir("../public"))
 	http.Handle("/", fs)
 
-	// Configure websocket route
+	
 	http.HandleFunc("/ws", handleConnections)
 
-	// Start listening for incoming chat messages
+	
 	go handleMessages()
 
-	// Start the server on localhost port 8010 and log any errors
+	
 	log.Println("http server started on :8010")
 	err := http.ListenAndServe(":8010", nil)
 	if err != nil {
@@ -43,32 +39,30 @@ func main() {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// Upgrade initial GET request to a websocket
+	
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Make sure we close the connection when the function returns
+	
 	defer ws.Close()
 
-	// Register our new client
-	//clients[ws] = true
+	
 
 	for {
 		var msg Message
-		// Read in a new message as JSON and map it to a Message object
+		
 		err := ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
-			//delete(clients, ws)
+			
 			break
 		}
 log.Printf(msg.Amount);
 log.Printf(msg.Adress);
-//log.Printf(msg.Message);
+
 		
-		// Send the newly received message to the broadcast channel
-		//broadcast <- msg
+		
 	}
 }
 
